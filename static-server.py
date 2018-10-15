6d8ef725d3aeb4bcb18373c729990a4dd70a41ba
Corey <coreyarice@gmail.com>
2018-10-14 22:31:38 -0500

    Update to python3

--- static-server.py
+++ static-server.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python2
-from SimpleHTTPServer import SimpleHTTPRequestHandler
-import BaseHTTPServer
+#!/usr/bin/env python3
+from http.server import SimpleHTTPRequestHandler
+import http.server
 
 class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
@@ -8,4 +8,4 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
-    BaseHTTPServer.test(CORSRequestHandler, BaseHTTPServer.HTTPServer)
+    http.server.test(CORSRequestHandler, http.server.HTTPServer)
